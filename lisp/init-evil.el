9ab6d84158c08402df8069383c31c040fb6fbdf7
Ev2le0 <873677408@qq.com>
2019-06-06 16:51:35 +0800

    test rest

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -25,5 +25,5 @@
 (define-key evil-normal-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
 (define-key evil-visual-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
 
-
+dsdfsdfsdf
 (provide 'init-evil) 
