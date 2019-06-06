d3c77b30a5f911d800f5db03f12abe8c8652835e
Ev2le0 <873677408@qq.com>
2019-06-06 10:28:07 +0800

    moudlar config

--- init.el
+++ init.el
@@ -3,15 +3,15 @@
 (require 'dired-x)
 (require 'org-tempo)
 (require 'popwin)
+(require 'iedit)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; modular config
 (require 'init-func)
 (require 'init-packages)
 (require 'init-better-defaults)
-(require 'init-keybindings)
 (require 'init-ui)
 (require 'init-org)
-
+(require 'init-keybindings)
 (setq custom-file (expand-file-name "lisp/init-custom.el" user-emacs-directory))
 (load-file custom-file)
